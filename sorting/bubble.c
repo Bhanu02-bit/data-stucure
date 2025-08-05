8c121fa8c7eed23f2ddcd34dbd1bd83724fda0b7
Bhanu02-bit <aasambhanureddy@gmail.com>
2025-08-05 18:34:08 +0530

    bubble

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
-void inputarray(int n,int a[]){
-    printf("enter array:");
-    scanf("%d",a[n]);
-}
-void bubblesort(int arr[],int n){
+int main(){
+  int arr[7]={6,2,8,4,5,3,7},temp;
+  int n=7;
+
+  for(int i=0;i<n;i++){
+    printf("%d",arr[i]);
+  }
+
+ 
   for(int i=0;i<n-1;i++){
-    for(int j=0;j<n-i-1;j++){
-        if(arr[j]>arr[j+1]){
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-        }
+    for(int j=0;j<n-i;j++){
+      if(arr[j]>arr[j+1]){
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }
     }
   }
-}
+   printf("the output using bubble sort:");
+  for(int i=0;i<n;i++){
+    printf("%d",arr[i]);
+  }
 
-int main(){
- inputarray(5);
-
+  return 0;
 }
